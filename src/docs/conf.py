379b367ea37548d8dcdcb86b4c6b286133170bec
Tristan <tristan.salles@sydney.edu.au>
2020-03-11 10:03:49 +1100

    change version number

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -39,7 +39,7 @@ author = u'Tristan Salles'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.0.1'
+release = u'0.0.8'
 
 
 # -- General configuration ---------------------------------------------------
