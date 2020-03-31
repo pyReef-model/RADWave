2f9b116351c85fa55b84018453d4c01d4e580db6
Tristan Salles <tristan.salles@sydney.edu.au>
2020-03-31 11:32:32 +1100

    update version number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         author="Tristan Salles",
         author_email="tristan.salles@sydney.edu.au",
         url="https://github.com/pyReef-model/RADWave",
-        version="0.1.2",
+        version="1.0.0",
         description="A Python interface to perform wave analysis from satellite altimeter data.",
         long_description=long_description,
         long_description_content_type="text/markdown",
